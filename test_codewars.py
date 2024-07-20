9833c1db295c5cde25e3ea88e9c6d38358db93bb
m1 <uzbzarif@gmail.com>
2024-07-20 10:07:25 +0500

    tes for username

--- test_codewars.py
+++ test_codewars.py
@@ -1,5 +1,13 @@
 from codewars import User
 
-def test_get_total():
+
+def test_check_username():
     user = User('Naxalov')
+    assert user.check_username() == False,' Username does not exist'
+    user = User('naxalov')== True,'Correct username'
+
+def test_get_total():
+    user = User('naxalov')
     assert user.get_total() == 426, 'Should be 426'
+
+# Test for check_username
