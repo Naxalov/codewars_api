c8ddc65263343140699be957d787ffeb4a283c1f
m1 <uzbzarif@gmail.com>
2024-07-30 10:20:21 +0500

    get total of users

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from codewars import User
+import csv
 
-user = User('naxalov')
-# Check username
-print(user.check_username())
-print(user.data)
-# Get total completed kata
-print(user.get_total())
-# Get name of the user
-print(user.get_name())
+# Read data from csv file
+with open('codewars.csv') as csv_file:
+    # Read data from csv file and skip header
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    # Skip header
+    next(csv_reader)
+    for row in csv_reader:
+        username = row[2]
+        user = User(username)
+        print(user.get_total())
+    
